16c2d44c4bd3a767667e941334a4091976069b0a
Sam Boylett <sam@boylett.com>
2022-11-06 20:35:15 +0000

    Add more story

--- src/index.stories.tsx
+++ src/index.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta } from '@storybook/react';
+import { useState } from 'react';
 import { createFastContext } from './createFastContext';
 
 interface Value {
@@ -24,3 +25,33 @@ export const DefaultConsumer = () => (
         )}
     </context.Consumer>
 )
+
+export const ProviderWithConsumer = () => {
+    const [foo, setFoo] = useState<string>("");
+    const [bar, setBar] = useState<number>(0);
+    const [shouldUpdate, setShouldUpdate] = useState<boolean>(true);
+
+    return (
+        <context.Provider value={{ foo, bar }}>
+            <label>
+                Foo <input value={foo} onChange={e => setFoo(e.target.value)} />
+            </label>
+            
+            <label>
+                Bar <input value={bar} type="number" onChange={e => setBar(parseInt(e.target.value))} />
+            </label>
+            
+            <label>
+                Should update <input checked={shouldUpdate} type="checkbox" onChange={e => setShouldUpdate(e.target.checked)} />
+            </label>
+
+            <context.Consumer shouldUpdate={() => shouldUpdate}>
+                {value => (
+                    <pre>
+                        {JSON.stringify(value, null, 2)}
+                    </pre>
+                )}
+            </context.Consumer>
+        </context.Provider>
+    )
+}
